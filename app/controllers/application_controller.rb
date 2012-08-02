5ede90eff3785268b6f66a06a8a38fad1a5c6b45
Raghu Betina <rbetina@gmail.com>
2012-08-02 08:47:26 -0500

    Removed access token.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   
   helper_method :my_access_token
   def my_access_token
-    'AAACEdEose0cBALndLfu2JorbdzbOx5xNIaZAfEFqkb4LkZCqGCkuzhBgRYKebW4EkLrd6h8Bsx5uUQlsbzKEMft8GTryfaGzqtnljGswZDZD'
+    ''
   end
 end
